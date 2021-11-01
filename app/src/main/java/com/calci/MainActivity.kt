137924c0aa455ab2a2d08b5d25a378adf48e353e
gokul1630 <gokulgp3167@gmail.com>
2021-11-01 15:53:48 +0530

    code cleanup

--- app/src/main/java/com/calci/MainActivity.kt
+++ app/src/main/java/com/calci/MainActivity.kt
@@ -3,7 +3,6 @@ package com.calci
 import android.os.Bundle
 import android.util.Log
 import android.widget.HorizontalScrollView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.calci.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
@@ -16,34 +15,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.apply {
-        tv0.setOnClickListener { appendString("0", false) }
-        tv1.setOnClickListener { appendString("1", false) }
-        tv2.setOnClickListener { appendString("2", false) }
-        tv3.setOnClickListener { appendString("3", false) }
-        tv4.setOnClickListener { appendString("4", false) }
-        tv5.setOnClickListener { appendString("5", false) }
-        tv6.setOnClickListener { appendString("6", false) }
-        tv7.setOnClickListener { appendString("7", false) }
-        tv8.setOnClickListener { appendString("8", false) }
-        tv9.setOnClickListener { appendString("9", false) }
-        startBracket.setOnClickListener { appendOperator("(") }
+        tv0.setOnClickListener { appendString("0") }
+        tv1.setOnClickListener { appendString("1") }
+        tv2.setOnClickListener { appendString("2") }
+        tv3.setOnClickListener { appendString("3") }
+        tv4.setOnClickListener { appendString("4") }
+        tv5.setOnClickListener { appendString("5") }
+        tv6.setOnClickListener { appendString("6") }
+        tv7.setOnClickListener { appendString("7") }
+        tv8.setOnClickListener { appendString("8") }
+        tv9.setOnClickListener { appendString("9") }
+        startBracket.setOnClickListener { appendString("(") }
         endBracket.setOnClickListener { appendOperator(")") }
         add.setOnClickListener { appendOperator("+") }
         sub.setOnClickListener { appendOperator("-") }
         tvDot.setOnClickListener { appendOperator(".") }
         clear.setOnClickListener { appendString("", true) }
-        divide.setOnClickListener {
-            if(calculation.text.isNotEmpty()){
-                appendString("/", false)
-            }
-        }
-
-        multiply.setOnClickListener {
-            if(calculation.text.isNotEmpty()){
-            appendString("*", false)
-            }
-        }
-
+        divide.setOnClickListener { appendOperator("/") }
+        multiply.setOnClickListener { appendOperator("*") }
 
         back.setOnClickListener {
             val trim = calculation.text
@@ -53,31 +42,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        equals.setOnClickListener {
-            try {
-                val exp = ExpressionBuilder(calculation.text.toString()).build()
-                val result = exp.evaluate()
-                val longResult = result.toLong()
-                if (result == longResult.toDouble()) {
-                    answer.text = longResult.toString()
-                    Log.w("result", "if == $longResult")
-                } else {
-                    answer.text = result.toString()
-                    Log.w("result", "else == $result")
-                }
-            }catch(e:Exception){
-                Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_SHORT).show()
-            }
-        }
     }
     }
     private fun appendOperator(operator:String){
         val text=binding.calculation.text
         if(text.isNotEmpty() && text.substring(text.length-1)!=operator){
-            appendString(operator,false)
+            appendString(operator)
         }
     }
-    private fun appendString(text:String,isClear:Boolean) {
+    private fun appendString(text:String,isClear:Boolean=false) {
         binding.apply {
             if (isClear) {
                 calculation.text = ""
@@ -87,6 +60,18 @@ class MainActivity : AppCompatActivity() {
                 calculationScroll.postDelayed({
                     calculationScroll.fullScroll(HorizontalScrollView.FOCUS_RIGHT)
                 },10)
+            }
+            try {
+                val exp = ExpressionBuilder(calculation.text.toString()).build()
+                val result = exp.evaluate()
+                val longResult = result.toLong()
+                if (result == longResult.toDouble()) {
+                    answer.text = longResult.toString()
+                } else {
+                    answer.text = result.toString()
+                }
+            }catch(error:Exception){
+                Log.d("CALCULATION_ERROR",error.message.toString())
             }
         }
 
